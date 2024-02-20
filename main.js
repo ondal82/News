2dbe7c33091c73ba5cf15dbf205d11ce5a0fc473
ONDAL <lusci@naver.com>
2024-02-21 00:48:55 +0900

    test2

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const API_KEY_NewsApi = `cdd4b0ea8b224300ab35acd8f3ed4981`;
 
+let keyword = "";
+let 
+
+
 let news = [];
 
 const getLatestNews = async () => {
     const url = new URL (
         // `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY_NewsApi}`
         // `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
-        `https://news-on.netlify.app/`
+        `https://news-on.netlify.app/top-headlines`
+        // `https://news-on.netlify.app/top-headlines?q=${keyword}&country=kr&pagesize=${PAGE_SIZE}`
     );
     console.log("uuu",url);
     const response = await fetch(url);
